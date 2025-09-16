09a29a7020077dca7ea70dae3d19b78b5a3700dc
kirbyEATStrawberryCake <entozforme@gmail.com>
2025-09-16 10:00:05 +0900

    250916 보드 관련 클래스 충돌 수정
    보드 관련 클래스 충돌 수정

--- Assets/02_Scripts/AI/GomokuBoard.cs
+++ Assets/02_Scripts/AI/GomokuBoard.cs
@@ -1,39 +1,45 @@
 using System;
 using UnityEngine;
 
-public enum Stone
-{
-    Empty = 0,
-    Black = 1,
-    White = 2
-}
+// StoneType을 사용
+// public enum Stone
+// {
+//     Empty = 0,
+//     Black = 1,
+//     White = 2
+// }
 
 public class GomokuBoard
 {
-    public const int SIZE = 15;
-    private readonly Stone[,] _board = new Stone[SIZE, SIZE];
+    // public const int SIZE = 15;
+    // private readonly StoneType[,] _board = new StoneType[SIZE, SIZE];
 
-    public bool IsInside(int x, int y)
-    {
-        return x >= 0 && x < SIZE && y >= 0 && y < SIZE;
-    }
+    // BoardManager.IsValidPosition 로 대체
+    // public bool IsInside(int x, int y)
+    // {
+    //     return x >= 0 && x < SIZE && y >= 0 && y < SIZE;
+    // }
 
-    public Stone GetStone(int x, int y)
-    {
-        if (!IsInside(x, y))
-            throw new ArgumentOutOfRangeException($"({x},{y}) is outside board.");
-        return _board[x, y];
-    }
+    // BoardManager.GetStoneAt 로 대체
+    // public StoneType GetStone(int x, int y)
+    // {
+    //     if (!IsInside(x, y))
+    //         throw new ArgumentOutOfRangeException($"({x},{y}) is outside board.");
+    //     return board[x, y];
+    // }
 
-    public bool IsEmpty(int x, int y)
-    {
-        return IsInside(x, y) && _board[x, y] == Stone.Empty;
-    }
-
-    public void PlaceStone(int x, int y, Stone stone)
-    {
-        if (!IsInside(x, y))
-            throw new ArgumentOutOfRangeException($"({x},{y}) is outside board.");
-        _board[x, y] = stone;
-    }
+    // 
+    // public bool IsEmpty(int x, int y)
+    // {
+    //     return IsInside(x, y) && _board[x, y] == StoneType.None;
+    // }
+    
+    
+    //BoardManager.TryPlaceStone 로 대체
+    // public void PlaceStone(int x, int y, StoneType stone)
+    // {
+    //     if (!IsInside(x, y))
+    //         throw new ArgumentOutOfRangeException($"({x},{y}) is outside board.");
+    //     _board[x, y] = stone;
+    // }
 }
