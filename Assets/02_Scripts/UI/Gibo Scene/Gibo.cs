1fab5a74d10deb33412d6ea145035f425b53bfbe
kang-hyiyoung <68013740+kang-heeyoung@users.noreply.github.com>
2025-09-19 18:06:52 +0900

    [ADD] 기보 씬 추가
    - 내 정보 불러오는 부분 추가해야함

---
+++ Assets/02_Scripts/UI/Gibo Scene/Gibo.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[Serializable]
+//public class MoveData
+//{
+//    public int turn;          // 몇 번째 수
+//    public string stoneColor; // "black" or "white"
+//    public int x;             // 좌표 X
+//    public int y;             // 좌표 Y
+//}
+
+//[Serializable]
+//public class GameRecord
+//{
+//    public string startTime;     // 게임 시작 시간 (저장 키)
+//    public string blackPlayer;   // 흑 닉네임
+//    public string whitePlayer;   // 백 닉네임
+//    public int blackRank;        // 흑 급수
+//    public int whiteRank;        // 백 급수
+//    public List<MoveData> moves = new List<MoveData>(); // 수순 리스트
+//}
